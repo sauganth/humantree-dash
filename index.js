9d4434774f67e2236fb4a8303713156d86a5d119
Sauganth Vijayappan <sauganthv@gmail.com>
2015-09-19 19:23:11 -0500

    Add bower and grunt stuff

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = function(width, height) {
-  return width * height;
-};
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require('http');
+var dns = require('dns');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=UTF-8'
+    });
+
+    res.write('Hello from Hello World.\n');
+    dns.lookup('www.google.com', function onLookup(err, addresses, family) {
+        console.log('addresses:', addresses);
+        res.write('addresses:'+String(addresses));
+        res.end();
+    });
+}).listen(9080, "");
